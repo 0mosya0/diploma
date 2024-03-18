1a42eee62c58be5001be4bcb7c5602c1e5aaf5d0
Ilya <ilha.eliseenko@yandex.by>
2024-03-19 00:03:00 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
-<template>
-  <h1 class="text-3xl text-slate-500 text-center">
-    Hello
-  </h1>
+<script>
+import Header from "./components/Header.vue";
 
+export default {
+   name: "App",
+   components: {
+      Header,
+   },
+};
+</script>
+
+<template>
+   <Header></Header>
+   <div class="text-center mt-4">
+      <router-link to="/home"> Home </router-link>
+      <router-link to="/login"> Login </router-link>
+      <router-view />
+   </div>
 </template>
